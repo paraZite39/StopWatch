a7b2527dd1143174af7461239bc8bcb0d72d00d1
paraZite39 <mezeidragos99@gmail.com>
2022-05-04 01:42:05 +0300

    added history detail frame

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-from datetime import datetime
 from datetime import date
 import tkinter as tk
 from tkinter import ttk
 from tkinter import simpledialog
-import sys
-import time
+from tkinter import messagebox
 
 
 class StopWatch:
     def __init__(self, watch_root, hist):
         """
         Initializes stopwatch
-        :param root: tk root in which to put the elements
+        :param watch_root: tk root in which to put the elements
         :param hist: stopwatch history in which to store records
         """
         self.hist = hist
@@ -105,6 +103,10 @@ class StopWatch:
         """
         Saves current timer (prompts for category) and resets
         """
+        if self.seconds == 0:
+            messagebox.showerror(title="Empty Save", message="Cannot save record of 0 seconds!")
+            return
+
         if self.running:
             self.running = False
 
@@ -120,18 +122,59 @@ class StopWatch:
 
 class StopWatchHistory:
     def __init__(self, hist_root):
+        """
+        Initializes stopwatch history
+        :param hist_root: the root in which to show the history
+        """
+        self.hist_root = hist_root
         self.history = []
         self.index = 1
-        self.hist_gui = tk.Listbox(hist_root, width=30)
+        self.hist_gui = tk.Listbox(hist_root, width=45)
         self.hist_gui.pack(side=tk.LEFT)
 
+        # frame on the right side (details)
+        self.f = tk.Frame(hist_root, height=220)
+        self.f.pack(side=tk.LEFT)
+        self.title_lab = tk.Label(self.f, text='Category: ', font=('Arial', 18))
+        self.title_lab.pack()
+        self.date_lab = tk.Label(self.f, text='Date: ', font=('Arial', 15))
+        self.date_lab.pack()
+        self.length_lab = tk.Label(self.f, text='Length: ', font=('Arial', 15))
+        self.length_lab.pack()
+
+        self.delete_button = tk.Button(self.f, text='Delete', command=lambda: print('delete'))
+        self.delete_button.pack()
+
+        self.hist_gui.bind("<<ListboxSelect>>", self.onselect)
+
+    def onselect(self, e):
+        w = e.widget
+        ind = int(w.curselection()[0])
+        record = w.get(ind).split(' | ')
+        print(record)
+        self.title_lab.configure(text='Category: ' + record[-1])
+        self.date_lab.configure(text='Date: ' + record[0])
+        self.length_lab.configure(text='Length: ' + record[1])
+
     def add_record(self, record):
+        """
+        Enters record into the history
+        :param record: the record to be introduced
+        """
         self.hist_gui.insert(self.index, record)
         self.index += 1
+
+    def delete_record(self, ind):
+        pass
 
 
 class MainApp:
     def __init__(self, main_root):
+        """
+        Initializes main stopwatch app
+        :param main_root: the root in which to display the app
+        """
+        # two tabs - stopwatch and history
         self.tab_control = ttk.Notebook(main_root)
         self.tab1 = ttk.Frame(self.tab_control)
         self.tab2 = ttk.Frame(self.tab_control)
@@ -144,9 +187,10 @@ class MainApp:
         self.stopwatch = StopWatch(self.tab1, self.history)
 
 
-root = tk.Tk()
-root.geometry('486x220')
-root.title('Stopwatch')
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.geometry('486x220')
+    root.title('Stopwatch')
 
-app = MainApp(root)
-root.mainloop()
+    app = MainApp(root)
+    root.mainloop()
